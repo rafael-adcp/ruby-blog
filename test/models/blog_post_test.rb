84a2f0b971f7507ecdc04b6d05206e50d12051e9
Rafael Prado <rafael_adcp@hotmail.com>
2025-04-16 12:47:28 -0300

    unit testing

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft? returns true for blog post" do
+    # both do the same thing
+    assert_equal BlogPost.new(published_at: nil).draft?, true
+    assert BlogPost.new(published_at: nil).draft?
+  end
+
+  test "draft? returns false for a published blog post" do
+    # both do the same thing
+    assert_equal BlogPost.new(published_at: 1.year.ago).draft?, false
+    refute BlogPost.new(published_at: 1.year.ago).draft?
+  end
+
+
+  test "draft? returns false for a scheduled blog post" do
+    # both do the same thing
+    assert_equal BlogPost.new(published_at: 1.year.from_now).draft?, false
+    refute BlogPost.new(published_at: 1.year.from_now).draft?
+  end
+  # ...
+  # ...
+  # ...
+  # ...
+  # using fixtures
+  test "[using fixture] draft? returns true for blog post" do
+    # blog_posts is the name of the yaml file where the fixture is
+    assert blog_posts(:draft).draft?
+  end
 end
